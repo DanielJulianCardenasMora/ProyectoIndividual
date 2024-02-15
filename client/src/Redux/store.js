e7082bbe89eb3525f899e7975502d86d8d0b5222
danieljulian <danielcamora@hotmail.com>
2024-02-15 16:09:48 -0500

    Componiendo front

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,4 +1,21 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./reducer";
 
+
+
+
+
+
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancer(applyMiddleware(thunkMiddleware))
+)
+
+export default store;
 
 
 
