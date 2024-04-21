e45dcb808bcffab2f86757d8507c3e75b5ef65ce
danieljulian <danielcamora@hotmail.com>
2024-04-21 18:16:48 -0500

    a

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ const server = express();
 
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://proyecto-individual-on6k6buq2.vercel.app/', 'http://localhost:5173');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
      'Access-Control-Allow-Headers',
