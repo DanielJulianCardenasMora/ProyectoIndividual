f8e8a7e8591060002b109237f9557a69e5dc15fe
danieljulian <danielcamora@hotmail.com>
2024-04-22 13:36:20 -0500

    -

---
+++ api/config.js
@@ -0,0 +1,27 @@
+const PORT = process.env.PORT || 3001
+
+// const EMAILER_HOST = process.env.EMAILER_HOST;
+// const EMAILER_PORT = process.env.EMAILER_PORT;
+// const EMAILER_USER = process.env.EMAILER_USER;
+// const EMAILER_PASSWORD = process.env.EMAILER_PASSWORD;
+
+const DB_USER = process.env.DB_USER || process.env.LOCAL_DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD || process.env.LOCAL_DB_PASSWORD
+const DB_HOST = process.env.DB_HOST || process.env.LOCAL_DB_HOST
+const DB_NAME = process.env.DB_NAME || process.env.LOCAL_DB_NAME
+const DB_PORT = process.env.DB_PORT || process.env.LOCAL_DB_PORT
+// const HOST = process.env.HOST || process.env.LOCAL_HOST
+
+module.exports = {
+    PORT,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_USER,
+    DB_HOST,
+    HOST,
+    // EMAILER_HOST,
+    // EMAILER_PORT,
+    // EMAILER_USER,
+    // EMAILER_PASSWORD,
+}
--- api/src/db.js
+++ api/src/db.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DATABASE_URL } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DATABASE_URL } = require('../config');
 const gameModel = require("./models/Game");
 const genreModel = require("./models/Genre");
 
@@ -8,22 +8,23 @@ const genreModel = require("./models/Genre");
 //   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
 //   { logging: false, native: false }
 // );
-const sequelize = new Sequelize(
-  DATABASE_URL,
-  { logging: false, native: false }
-);
 
-// let sequelize = new Sequelize({
-//   database: DB_NAME,
-//   username: DB_USER,
-//   password: DB_PASSWORD,
-//   host: DB_HOST,
-//   port: DB_PORT,
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: false,
-//   },
-// });
+// const sequelize = new Sequelize(
+//   DATABASE_URL,
+//   { logging: false, native: false }
+// );
+
+let sequelize = new Sequelize({
+  database: DB_NAME,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: false,
+  },
+});
 
 gameModel(sequelize);
 genreModel(sequelize);
