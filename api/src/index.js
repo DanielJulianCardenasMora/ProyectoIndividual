bb2d461ac5d3b6c081f4507f6b65a8a200272dd2
danieljulian <danielcamora@hotmail.com>
2024-04-22 14:44:46 -0500

    -

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,14 @@
 const server = require("./app");
 const { sequelize } = require("./db");
 require("dotenv").config();
-const { PORT } = process.env.DB_PORT || 3001;
+// const { DB_PORT } = process.env || 3001;
+const { DB_PORT } = require('../config')
 
 sequelize
   .sync({ Force: true })
   .then(() => {
-    server.listen(PORT, () => {
-      console.log("Server listening on port:", PORT);
+    server.listen(DB_PORT, () => {
+      console.log("Server listening on port:", DB_PORT);
     });
   })
   .catch((err) => console.log(err));
