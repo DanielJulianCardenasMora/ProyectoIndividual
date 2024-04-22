567dfba55a883a57caf11e8b6fd664af52b34a36
danieljulian <danielcamora@hotmail.com>
2024-04-21 19:58:06 -0500

    -

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const router = require("./routes");
 const server = express();
+const cors = require("cors");
 
 
 
@@ -9,19 +10,27 @@ const server = express();
 
 
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://localhost:5173', 'https://proyecto-individual-gilt.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-     'Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-     'Access-Control-Allow-Methods',
-     'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header(
+//      'Access-Control-Allow-Headers',
+//      'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   res.header(
+//      'Access-Control-Allow-Methods',
+//      'GET, POST, OPTIONS, PUT, DELETE'
+//   );
+//   next();
+// });
+server.use(
+   cors({
+     origin: ["http://localhost:5173", "https://proyecto-individual-i9tee3m9h.vercel.app"],
+     credentials: true,
+     methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+     allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+   })
+ );
 
 server.use(express.json())
 server.use('/mundoVideoJuegos', router)
