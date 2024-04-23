04b6c28462981ac8102e9456466f6c1d55ecb0b3
danieljulian <danielcamora@hotmail.com>
2024-04-23 17:14:54 -0500

    -

--- api/src/db.js
+++ api/src/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DATABASE_URL, DB_PORT } = require('../config');
-// const { LOCAL_DB_USER, LOCAL_DB_PASSWORD, LOCAL_DB_HOST, LOCAL_DB_NAME, DATABASE_URL, DB_PORT } = process.env
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DATABASE_URL, DB_PORT } = require('../config');
+const { LOCAL_DB_USER, LOCAL_DB_PASSWORD, LOCAL_DB_HOST, LOCAL_DB_NAME, DATABASE_URL, DB_PORT } = process.env
 const gameModel = require("./models/Game");
 const genreModel = require("./models/Genre");
 const modelModel = require("./models/Model");
@@ -11,22 +11,22 @@ const modelModel = require("./models/Model");
 //   { logging: false, native: false }
 // );
 
-// const sequelize = new Sequelize(
-//   DATABASE_URL,
-//   { logging: false, native: false }
-// );
+const sequelize = new Sequelize(
+  DATABASE_URL,
+  { logging: false, native: false }
+);
 
-let sequelize = new Sequelize({
-  database: DB_NAME,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: false,
-  },
-});
+// let sequelize = new Sequelize({
+//   database: DB_NAME,
+//   username: DB_USER,
+//   password: DB_PASSWORD,
+//   host: DB_HOST,
+//   port: DB_PORT,
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: false,
+//   },
+// });
 
 gameModel(sequelize);
 genreModel(sequelize);
